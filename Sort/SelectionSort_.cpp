a36f06c4c334b6c431083f7b67e97b6c9df0de85
Lê Đình Nhuận <teo@.gmail.com>
2024-04-27 08:01:23 +0700

    Practice Selection Sort

--- Sort/SelectionSort_.cpp
+++ Sort/SelectionSort_.cpp
@@ -1,9 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void selectionSort(int a[], int n){
-	//tìm phần tử nhỏ nhất trong mảng chưa được sắp xếp
-	for(int i = 0; i < n - 1; i++){
+void input(int a[], int n){
+	for(int i = 0; i < n; i++){
+		a[i] = rand() % 10000 + 1;
+	}
+}
+
+void output(int a[], int n){
+	for(int i = 0; i < n; i++){
+		cout << a[i] << endl;
+	}
+	cout << endl;
+}
+
+void SelectionSort(int a[], int n){
+	for(int i = 0; i < n; i++){
 		int minIndex = i;
 		for(int j = i + 1; j < n; j++){
 			if(a[j] < a[minIndex]){
@@ -17,24 +29,13 @@ void selectionSort(int a[], int n){
 	}
 }
 
-void Print(int a[], int n){
-	for(int i = 0; i < n;i++){
-		cout << a[i] << "  ";
-	}
-	cout << endl;
-}
-
 int main(){
 	int n;
 	cin >> n;
 	int a[n];
-	
-	for(int i = 0; i < n; i++){
-		cin >> a[i];
-	}
-	
-	Print(a, n);
-	selectionSort(a, n);
-	Print(a, n);
+	input(a, n);
+	output(a, n);
+	SelectionSort(a, n);
+	output(a, n);
 	return 0;
 }
